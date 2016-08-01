a328f23b5b7fd3951a96cae9d8234ec889ee9ffd
McXinuS (PC) <mcxinus@mail.ru>
2016-08-01 20:09:26 +0800

    Attempt to fix error 310 (too many redirects)

--- index.js
+++ index.js
@@ -7,18 +7,17 @@ var express = require('express'),
 var server = require('http').Server(app);
 
 var port = process.env.PORT || 5000;
-
 server.listen(port);
 
 app.use(express.static('public'));
-app.use('/*', function(req, res, next) {  
+app.use('*', function(req, res, next) {  
   if(!req.secure) {
     var secureUrl = "https://" + req.headers['host'] + req.url; 
     res.writeHead(301, { "Location":  secureUrl });
     res.end();
 	console.log('Redirected to the https');
   }
-  next();
+  //next();
 });
 
 app.get('/', function (req, res) {

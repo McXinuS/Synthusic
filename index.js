1889430c952c13c475aac1d01c83be3027baab5a
McXinuS (PC) <mcxinus@mail.ru>
2016-08-01 18:42:24 +0800

    Force https connection

--- index.js
+++ index.js
@@ -4,17 +4,25 @@
 
 var express = require('express'),
   app = express();
-var server = require('https').Server(app);
+var server = require('http').Server(app);
 
-server.listen(process.env.PORT || 5000);
+var port = process.env.PORT || 5000;
+
+server.listen(port);
 
 app.use(express.static('public'));
-
-app.get('/index.html', function (req, res) {
-  res.sendFile(__dirname + '/index.html');
+app.use('/*', function(req, res, next) {  
+  if(!req.secure) {
+    var secureUrl = "https://" + req.headers['host'] + req.url; 
+    res.writeHead(301, { "Location":  secureUrl });
+    res.end();
+	console.log('Redirected to the https');
+  }
+  next();
 });
+
 app.get('/', function (req, res) {
   res.sendFile(__dirname + '/index.html');
 });
 
-console.log("The server is running @ localhost:8080");
+console.log("The server is running @ " + port);

d282d2ce35a447c35df6d0d9c470d1dfb6c3b036
McXinuS (PC) <mcxinus@mail.ru>
2016-08-02 01:38:50 +0800

    InsecurePort change to 4080

--- index.js
+++ index.js
@@ -7,24 +7,18 @@ var express = require('express'),
   app = express();
 
 var port = process.env.PORT || 8443;
-var insecurePort = process.argv[3] || 5000;
+var insecurePort = process.argv[3] || 4080;
 
 var options = {
   key: fs.readFileSync('ssl/simple-synthesizer_herokuapp_com.key'),
   cert: fs.readFileSync('ssl/simple-synthesizer_herokuapp_com.crt')
 };
 
-var server = require('https').createServer(options, app);
-/*
-require("https").createServer(options, function(req, res){
-    res.writeHead(200, {    'Content-Type': 'text/plain', 
-    "Strict-Transport-Security": "max-age=604800"});
-    res.end('Hello from SSL!\n');
-  }).listen(port);
-*/
+var server = require('https').createServer(options, app);  
+server.listen(port);
 
 // redirect to HTTPS
-require("http").Server(function(req, res){
+var server = require("http").Server(function(req, res){
 	// remove port from host name
 	var hostname = ( req.headers.host.match(/:/g) )
 		? req.headers.host.slice( 0, req.headers.host.indexOf(":") )
@@ -37,15 +31,14 @@ require("http").Server(function(req, res){
     res.end('Redirecting to SSL\n');
 	
 	var oldUrl = req.protocol + '://' + req.headers.host + req.url;
-	console.log('Redirected to the https from "' + oldUrl + '" to "' + newUrl + '"');
+	console.log('Redirecting to SSL: from "' + oldUrl + '" to "' + newUrl + '"');
   }).listen(insecurePort);
 
-  
-server.listen(port);
 
 app.use(express.static('public'));
 
 app.get('/', function (req, res) {
+  console.log('Responsing to "' + req.url + '" request');
   res.sendFile(__dirname + '/index.html');
 });
 

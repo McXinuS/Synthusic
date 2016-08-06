e625dd0c03a69980cb11bd41f66bab4e7c232a9f
McXinuS (PC) <mcxinus@mail.ru>
2016-08-06 14:38:27 +0800

    https

--- index.js
+++ index.js
@@ -6,23 +6,26 @@ var fs = require('fs');
 var express = require('express'),
   app = express();
 
-var port = process.env.PORT || 8443;
-var insecurePort = process.argv[3] || 4080;
+var port = process.env.PORT || 5000;
+//var insecurePort = process.argv[3] || 4080;
 
-var options = {
+require("http").Server(app).listen(port);
+
+/*var options = {
   key: fs.readFileSync('ssl/simple-synthesizer_herokuapp_com.key'),
   cert: fs.readFileSync('ssl/simple-synthesizer_herokuapp_com.crt')
-};
+};*/
 
-var server = require('https').createServer(options, app);  
-server.listen(port);
+//var server = require('https').createServer(options, app);  
+//server.listen(port);
 
 // redirect to HTTPS
-var server = require("http").Server(function(req, res){
+/*var server = require("http").Server(function(req, res){
 	// remove port from host name
 	var hostname = ( req.headers.host.match(/:/g) )
 		? req.headers.host.slice( 0, req.headers.host.indexOf(":") )
 		: req.headers.host;
+		
 	var newUrl = 'https://' + hostname + ':' + port + req.url;
     res.writeHead(301, {
        'Content-Type': 'text/plain', 
@@ -32,13 +35,39 @@ var server = require("http").Server(function(req, res){
 	
 	var oldUrl = req.protocol + '://' + req.headers.host + req.url;
 	console.log('Redirecting to SSL: from "' + oldUrl + '" to "' + newUrl + '"');
-  }).listen(insecurePort);
+  }).listen(insecurePort);*/
 
 
 app.use(express.static('public'));
 
+app.get('*', function(req,res,next){
+  if(req.headers['x-forwarded-proto']!='https'){
+    console.log('Redirecting to HTTPS');
+    res.redirect('https://'+req.headers.host+req.url);
+  }
+  else{
+    console.log('Responsing to "' + req.url + '" request');
+    next();
+  }
+})
+
+/*app.all('*', function (req, res, next) {
+  if (req.protocol !== 'https'){
+    console.log('redirecting to HTTPS...');
+    var newUrl = 'https://' + req.headers.host + req.url;
+    res.writeHead(301, {
+      'Content-Type': 'text/plain', 
+      'Location': newUrl
+    });
+    res.end('Redirecting to SSL\n');
+  }
+  else{
+    console.log('Responsing to "' + req.url + '" request');
+    next();
+  }
+});*/
+
 app.get('/', function (req, res) {
-  console.log('Responsing to "' + req.url + '" request');
   res.sendFile(__dirname + '/index.html');
 });
 

21cfdb1841477030fef2040d98ced0e9f6a6a009
McXinuS (PC) <mcxinus@mail.ru>
2016-09-12 00:54:24 +0800

    Check nodejs arguments for undefined

--- index.js
+++ index.js
@@ -9,7 +9,9 @@ require("http").Server(app).listen(port);
 
 app.use(express.static('public'));
 
-var useSsl = (process.argv[2].toLowerCase()=='true') || false;
+var useSsl = (process.argv[2] != undefined 
+	|| process.argv[2].toLowerCase()=='true') 
+	|| false;
 
 // redirect to HTTPS
 if (useSsl) {

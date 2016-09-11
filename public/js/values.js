ac704bad12ef6375521801e212a62e73188e01e5
McXinuS (PC) <mcxinus@mail.ru>
2016-09-12 00:50:48 +0800

    Accidentally left these files untracked

---
+++ public/js/values.js
@@ -0,0 +1,55 @@
+var NOTE_START = ['C', 2];
+var NOTE_END = ['B', 5];
+
+var MAX_GAIN = 0.5;
+
+var INSTRUMENTS = {
+	piano: {
+		name: 'Piano',
+		osc_count: 4,
+		osc_type: ['sine', 'sine', 'square', 'sine'],
+		osc_freq: [1, 2, 0.5, 1.66],
+		osc_gain: [1, 0.4, 0.1, 0.2]
+	},
+	test_instr: {
+		name: 'Test',
+		osc_count: 2,
+		osc_type: ['sine', 'sine'],
+		osc_freq: [1, 2],
+		osc_gain: [1, 0.4]
+	}
+};
+
+var ACCIDENTALS = {
+	natural: {
+		name: 'Natural',
+		scale: ['C', 'D', 'E', 'F', 'G', 'A', 'B']
+	},
+	sharp: {
+		name: 'Sharp',
+		scale: ['C', 'C♯', 'D', 'D♯', 'E', 'F', 'F♯', 'G', 'G♯', 'A', 'A♯', 'B']
+	},
+	flat: {
+		name: 'Flat',
+		scale: ['C', 'D♭', 'D', 'E♭', 'E', 'F', 'G♭', 'G', 'A♭', 'A', 'B♭', 'B']
+	}
+};
+
+var SCALES = {
+	minor: {
+		name: 'Minor',
+		intervals: [2, 1, 2, 2, 1, 2, 2]
+	},
+	major: {
+		name: 'Major',
+		intervals: [2, 2, 1, 2, 2, 2, 1]
+	},
+	pentatonicMinor: {
+		name: 'Pentatonic minor',
+		intervals: [3, 2, 3, 2, 2]
+	},
+	pentatonicMajor: {
+		name: 'Pentatonic major',
+		intervals: [2, 3, 2, 2, 3]
+	}
+};

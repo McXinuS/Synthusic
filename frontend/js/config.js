392fe7346d60f985b20a9647c46b6423cb4362fe
McXinuS (PC) <mcxinus@mail.ru>
2016-11-05 22:40:31 +0800

    Added triangular wave function

--- frontend/js/config.js
+++ frontend/js/config.js
@@ -35,11 +35,15 @@ function Constants() {
 				return Math.sin;
 			case 'square':
 				return function (t) {
-					return (Math.sin(t) > 0) ? 1 : -1
+					return t == 0 ? 0 : (Math.sin(t) > 0) ? 1 : -1
 				};
 			case 'sawtooth':
 				return function (t) {
 					return t > 0 ? 2 * (t % 1) - 1 : 2 * (t % 1) + 1;
+				};
+			case 'triangle':
+				return function (t) {
+					return t > 0 ? 4 * Math.abs(t % 1 - 0.5) - 1 : 4 * Math.abs(t % 1 + 0.5) - 1;
 				};
 			default:
 				return Math.sin;
@@ -72,6 +76,13 @@ function Constants() {
 			name: 'Sawtooth',
 			osc_count: 1,
 			osc_type: ['sawtooth'],
+			osc_freq: [1],
+			osc_gain: [1]
+		},
+		triangle: {
+			name: 'Triangle',
+			osc_count: 1,
+			osc_type: ['triangle'],
 			osc_freq: [1],
 			osc_gain: [1]
 		}

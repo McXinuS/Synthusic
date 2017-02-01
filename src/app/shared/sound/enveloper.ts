2988ea6090b3707342c8ec9f71044eab51f54356
McXinuS (PC) <mcxinus@mail.ru>
2017-02-01 23:22:11 +0700

    Multiple instrument in sound module, enveloper

---
+++ src/app/shared/sound/enveloper.ts
@@ -0,0 +1,94 @@
+import {Envelope} from "../instrument/envelope.model";
+export enum EnveloperState {
+  STATE_STARTED,
+  STATE_DECAYING,
+  STATE_SUSTAINED,
+  STATE_RELEASING,
+  STATE_FINISHED
+}
+
+export class Enveloper {
+
+  audioCtx: AudioContext;
+  gainNode: GainNode;
+  envelope: Envelope;
+  state: EnveloperState = EnveloperState.STATE_FINISHED;
+  funcTimeoutId: number = -1;
+  onFinished: () => void;
+
+  constructor(audioCtx: AudioContext, envelope: Envelope, onFinished?: ()=>any) {
+    this.audioCtx = audioCtx;
+    this.gainNode = audioCtx.createGain();
+    this.gainNode.gain.value = 0;
+
+    this.envelope = envelope;
+
+    this.onFinished = onFinished;
+  }
+
+  connect(destination) {
+    this.gainNode.connect(destination);
+  };
+
+  updateState(type) {
+    switch (type) {
+      case EnveloperState.STATE_STARTED:
+        this.state = EnveloperState.STATE_STARTED;
+        this.cancelStateChange();
+        this.changeStateAtTime(EnveloperState.STATE_DECAYING, 1.0, this.envelope.attack);
+        break;
+      case EnveloperState.STATE_DECAYING:
+        this.state = EnveloperState.STATE_DECAYING;
+        this.changeStateAtTime(EnveloperState.STATE_SUSTAINED, this.envelope.sustain, this.envelope.decay);
+        break;
+      case EnveloperState.STATE_SUSTAINED:
+        this.state = EnveloperState.STATE_SUSTAINED;
+        break;
+      case EnveloperState.STATE_RELEASING:
+        this.state = EnveloperState.STATE_RELEASING;
+        this.cancelStateChange();
+        this.changeStateAtTime(EnveloperState.STATE_FINISHED, 0, this.envelope.release);
+        break;
+      case EnveloperState.STATE_FINISHED:
+        this.state = EnveloperState.STATE_FINISHED;
+        if (typeof(this.onFinished) == 'function') this.onFinished();
+        break;
+    }
+  };
+
+  changeStateAtTime(newState: EnveloperState, newGain: number, time: number) {
+    this.gainNode.gain.setValueAtTime(this.gainNode.gain.value, this.getRampTime(0));
+    this.gainNode.gain.linearRampToValueAtTime(newGain, this.getRampTime(time));
+    this.funcTimeoutId = setTimeout(this.updateState.bind(this, newState), time);
+  };
+
+  cancelStateChange() {
+    this.gainNode.gain.cancelScheduledValues(this.audioCtx.currentTime);
+    clearTimeout(this.funcTimeoutId);
+  };
+
+  start() {
+    this.updateState(EnveloperState.STATE_STARTED);
+  };
+
+  release() {
+    this.updateState(EnveloperState.STATE_RELEASING);
+  };
+
+  getGainNode() {
+    return this.gainNode;
+  };
+
+  getState() {
+    return this.state;
+  };
+
+  getGain() {
+    return this.gainNode.gain.value;
+  };
+
+// converts time from millis to seconds and adds it to the current time
+  private getRampTime(time) {
+    return this.audioCtx.currentTime + time / 1000.0;
+  };
+}
--- src/app/shared/sound/sound.service.ts
+++ src/app/shared/sound/sound.service.ts
@@ -4,6 +4,8 @@ import {Note} from "../note/note.model";
 import {SequencerService} from "../sequencer/sequencer.service";
 import {SequencerNote} from "../sequencer/sequencernote.model";
 import {BroadcastTopic} from "../broadcaster/broadcasttopic.enum";
+import {Enveloper} from "./enveloper";
+import {Instrument} from "../instrument/instrument.model";
 
 class GainedOscillatorNode extends OscillatorNode{
   gainNode: GainNode;
@@ -15,9 +17,11 @@ export class SoundService {
   audioContext: AudioContext;
   noteToStop: SequencerNote;
 
-  inputNode: AudioNode;
-
   masterGainNode: GainNode;
+  /**
+   * Contains enveloper gain nodes for every instrument.
+   */
+  envelopers: Map<number, Enveloper> = new Map();
 
   get masterGain() {
     return this.masterGainNode.gain.value;
@@ -70,7 +74,7 @@ export class SoundService {
   init() {
     this.audioContext = new AudioContext();
     this.masterGainNode = this.audioContext.createGain();
-    this.inputNode = this.masterGainNode;
+    this.masterGainNode.connect(this.audioContext.destination);
 
     this.broadcaster.on<SequencerNote>(BroadcastTopic.playNote)
       .subscribe(note => this.playNote(note));
@@ -81,12 +85,20 @@ export class SoundService {
   }
 
   private createOscillators(note: SequencerNote): GainedOscillatorNode[] {
+    let inputNode;
+    if (!this.envelopers.has(note.instrument.id)) {
+      let enveloper = this.createEnvelope(note.instrument);
+      this.envelopers.set(note.instrument.id, enveloper);
+      inputNode = enveloper.getGainNode();
+    } else {
+      inputNode = this.envelopers.get(note.instrument.id).getGainNode();
+    }
+
     let oscillators = [];
     let basePitch = note.note.freq;
-
     for (let oscillator of note.instrument.oscillators) {
       let gainNode = this.audioContext.createGain();
-      gainNode.connect(this.inputNode);
+      gainNode.connect(inputNode);
       gainNode.gain.value = 0;
 
       let osc: GainedOscillatorNode = <GainedOscillatorNode>(this.audioContext.createOscillator());
@@ -113,32 +125,30 @@ export class SoundService {
       this.setGain(note, 1, true);
     }
 
-    //this.enveloper.start();
+    this.envelopers.get(note.instrument.id).start();
   };
 
   /**
    * @param note
    * @param forceStop Don't save the note to stop it later with envelope callback, stop it right now instead.
    */
-  stopNote = function (note, forceStop = false) {
+  stopNote(note: SequencerNote, forceStop: boolean = false) {
     if (!this.isPlaying(note)) return;
 
     // if the only note is stopped, release the enveloper
-    if (this.playingCount == 0 && !forceStop) {
-      //this.enveloper.release();
+    if (this.playingCountGlobal == 0 && !forceStop) {
+      this.envelopers.get(note.instrument.id).release();
       // the note will be stopped in enveloper's onFinished callback
       // we need to remember currently fading note to stop it
       // if some note will be played before enveloper released
       this.noteToStop = note;
     } else {
       this.setGain(note, 0, true);
-      setTimeout(function () {
-        this.stopNoteImmediately(note)
-      }.bind(this), this.RAMP_STOP_TIME);
+      setTimeout(() => this.stopNoteImmediately(note), this.RAMP_STOP_TIME);
     }
   };
 
-  private stopNoteImmediately = function (note: SequencerNote) {
+  private stopNoteImmediately(note: SequencerNote) {
     if (!this.isPlaying(note)) return;
 
     for (let j = 0; j < note.instrument.oscillators.length; j++) {
@@ -148,15 +158,15 @@ export class SoundService {
     this.oscillators.delete(note.id);
   };
 
-  stop = function () {
+  stop(instrumentId?: number) {
     this.noteToStop = undefined;
 
-    for (let [id, arr] of this.oscillators) {
-      for (let osc of arr) {
+    this.oscillators.forEach((oscArr: GainedOscillatorNode[], id: number) => {
+      for (let osc of oscArr) {
         osc.stop(0);
         osc.disconnect();
       }
-    }
+    });
     this.oscillators.clear();
   };
 
@@ -170,26 +180,30 @@ export class SoundService {
    If ramp is set to false, set the note's gain immediately
    Otherwise, set it in RAMP_STOP_TIME to prevent click effect
    */
-  setGain = function (note: SequencerNote, gain: number, ramp: boolean = false) {
+  setGain(note: SequencerNote, targetGain: number, ramp: boolean = false) {
     if (!this.isPlaying(note)) return;
 
-    if (!ramp) {
-      // set immediately
-      for (let j = 0; j < note.instrument.oscillators.length; j++) {
-        this.oscillators.get(note.id)[j].gainNode.gain.value = gain * note.instrument.oscillators[j].gain;
+    let oscArr = this.oscillators.get(note.id);
+    let gain;
+    let rampTime = this.audioContext.currentTime + this.RAMP_STOP_TIME / 1000.0;
+
+    for (let j = 0; j < note.instrument.oscillators.length; j++) {
+      gain = targetGain * note.instrument.oscillators[j].gain;
+      if (ramp) {
+        oscArr[j].gainNode.gain.linearRampToValueAtTime(gain, rampTime);
+      } else {
+        oscArr[j].gainNode.gain.value = gain;
       }
-    } else {
-      // grow gain to 'gain' value in RAMP_STOP_TIME milliseconds
-      for (let j = 0; j < note.instrument.oscillators.length; j++) {
-        this.oscillators.get(note.id)[j].gainNode.gain.linearRampToValueAtTime(
-            gain * note.instrument.oscillators[j].gain,
-            this.audioContext.currentTime + this.RAMP_STOP_TIME / 1000.0);
-      }
-      //console.log(this.audioContext.currentTime + this.RAMP_STOP_TIME / 1000.0);
     }
   };
 
   isPlaying(note: SequencerNote) {
     return this.playingLocal.has(note.id);
+  }
+
+  private createEnvelope(instrument: Instrument): Enveloper {
+    let enveloper = new Enveloper(this.audioContext, instrument.envelope, () => this.stop(instrument.id));
+    enveloper.connect(this.masterGainNode);
+    return enveloper;
   }
 }

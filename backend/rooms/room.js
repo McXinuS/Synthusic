b444c37e0c7970fa79861292721efc846e353e90
McXinuS (PC) <mcxinus@mail.ru>
2017-03-25 15:43:15 +0700

    Clear master before merging with angular version

--- backend/rooms/room.js
+++
@@ -1,7 +0,0 @@
-/*
- TODO
- addUser - assign user to a free room
- assignUser(id) - assign to the particular room
- removeUser - remove user from a room he was assigned
- userAction(parameters) - send message or change configuration of the room
- */
